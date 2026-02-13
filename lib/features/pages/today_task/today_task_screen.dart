0e7071e63d84213f65ca53cc3bdbf14fd5aeb805
Tasha <tasha290929@gmail.com>
2026-02-13 16:38:15 +0200

    today_task_screen

--- lib/features/pages/today_task/today_task_screen.dart
+++ lib/features/pages/today_task/today_task_screen.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/src/extension_instance.dart';
+import 'package:test_project/core/controllers/task_controller.dart';
+import 'package:test_project/features/ui/size_config.dart';
+import 'package:test_project/styles/app_styles.dart';
 
-class TodayTaskScreen extends StatefulWidget{
+class TodayTaskScreen extends StatefulWidget {
   const TodayTaskScreen({Key? key}) : super(key: key);
 
   @override
   State<TodayTaskScreen> createState() => _TodayTaskScreen();
-
 }
 
 class _TodayTaskScreen extends State<TodayTaskScreen> {
+  DateTime _selectedDate = DateTime.now();
+  final TaskController _taskController = Get.put(TaskController());
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    SizeConfig().init(context);
+
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      appBar: _customAppBar(),
+      body: Column(
+        children: [
+          _addTaskBar(),
+          _addDateBar(),
+          const SizedBox(height: 8),
+          _showTasks(),
+        ],
+      ),
+    );
   }
 
+  AppBar _customAppBar(){
+    return AppBar(
+      leading: IconButton(
+          onPressed: onPressed,
+          icon: icon
+      ),
+    )
+  }
 }
